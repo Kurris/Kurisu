4ce3cfe498d3994621345196874cdabf96f0d58e
Kurris <791444095@qq.com>
2022-07-26 15:06:10 +0800

    feat 管道异常类pack

---
+++ Kurisu/Startup/AppPacks/DefaultGlobalExceptionPack.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Kurisu.Startup.Abstractions;
+using Kurisu.UnifyResultAndValidation.Abstractions;
+using Kurisu.Utils.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kurisu.Startup.AppPacks
+{
+    public class DefaultGlobalExceptionPack : BaseAppPack
+    {
+        public override int Order => 0;
+        public override bool IsBeforeUseRouting => true;
+
+        public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<GlobalExceptionMiddleware>();
+        }
+    }
+
+    public class GlobalExceptionMiddleware : BaseMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionMiddleware(RequestDelegate next) : base(next)
+        {
+            _next = next;
+        }
+
+        public override async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.Message;
+                context.Response.StatusCode = ex.Source.Contains("IdentityModel.AspNetCore") ? 401 : 500;
+
+                var apiResult = context.RequestServices.GetService<IApiResult>();
+
+                byte[] content = Encoding.UTF8.GetBytes(apiResult.GetDefaultErrorApiResult(errorMessage).ToJson());
+
+                context.Response.ContentType = "application/json";
+                context.Response.ContentLength = content.Length;
+                await context.Response.BodyWriter.WriteAsync(new ReadOnlyMemory<byte>(content));
+            }
+        }
+    }
+}
