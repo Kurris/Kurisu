80d21cc4b10b691060cb0aa8a130474c068e6a59
liguoyang <Ligy.97@foxmail.com>
2023-12-25 16:05:13 +0800

    feat 健康检查

---
+++ Kurisu.AspNetCore/Startup/AppPacks/DefualtHealthcheck.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kurisu.AspNetCore.Startup.AppPacks;
+
+/// <summary>
+/// 健康检查
+/// </summary>
+public class DefualtHealthcheck : BaseAppPack
+{
+    public override bool IsBeforeUseRouting { get; }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHealthChecks();
+    }
+
+    public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseHealthChecks("/healthz");
+    }
+}
