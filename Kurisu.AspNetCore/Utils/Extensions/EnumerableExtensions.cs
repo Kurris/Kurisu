136cec6b1d55e52232be51d7bb796a220afbd706
liguoyang <Ligy.97@foxmail.com>
2024-03-07 14:17:28 +0800

    feat 接口处理可递归结构

--- Kurisu.AspNetCore/Utils/Extensions/EnumerableExtensions.cs
+++ Kurisu.AspNetCore/Utils/Extensions/EnumerableExtensions.cs
@@ -9,16 +9,38 @@ namespace Kurisu.AspNetCore.Utils.Extensions;
 /// </summary>
 public static class EnumerableExtensions
 {
+    /// <summary>
+    /// 集合string join
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="separator"></param>
+    /// <returns></returns>
     public static string Join<T>(this IEnumerable<T> enumerable, string separator)
     {
         return string.Join(separator, enumerable);
     }
 
+    /// <summary>
+    /// 集合string join
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="separator"></param>
+    /// <returns></returns>
     public static string Join<T>(this IEnumerable<T> enumerable, char separator)
     {
         return string.Join(separator, enumerable);
     }
 
+    /// <summary>
+    /// 过滤条件
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <param name="condition"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
     public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> enumerable, bool condition, Func<T, bool> predicate)
     {
         return condition
@@ -26,7 +48,12 @@ public static class EnumerableExtensions
             : enumerable;
     }
 
-
+    /// <summary>
+    /// 为空
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <returns></returns>
     public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
     {
         if (enumerable == null)
@@ -37,8 +64,44 @@ public static class EnumerableExtensions
         return !enumerable.Any();
     }
 
+    /// <summary>
+    /// 存在
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumerable"></param>
+    /// <returns></returns>
     public static bool IsPresent<T>(this IEnumerable<T> enumerable)
     {
         return !enumerable.IsEmpty();
+    }
+
+    /// <summary>
+    /// 递归处理
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public static List<T> RecursionGetNext<T>(List<T> data) where T : IRecursionStruct<T>
+    {
+        var tops = data.Where(x => x.PCode.IsEmpty()).ToList();
+        tops.ForEach(current => current.RecursionGetNext(data));
+
+        return tops;
+    }
+
+    /// <summary>
+    /// 递归处理
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="current"></param>
+    /// <param name="data"></param>
+    public static void RecursionGetNext<T>(this T current, List<T> data) where T : IRecursionStruct<T>
+    {
+        var next = data.Where(x => x.PCode == current.Code).ToList();
+        current.Next = next;
+        foreach (var item in next)
+        {
+            RecursionGetNext(item, data);
+        }
     }
 }
---
+++ Kurisu.AspNetCore/Utils/IRecursionStruct.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Kurisu.AspNetCore.Utils;
+
+/// <summary>
+/// 可递归结构
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface IRecursionStruct<T>
+{
+    /// <summary>
+    /// 父级
+    /// </summary>
+    public string PCode { get; set; }
+
+    /// <summary>
+    /// 当前
+    /// </summary>
+    public string Code { get; set; }
+
+    /// <summary>
+    /// 下级
+    /// </summary>
+    public List<T> Next { get; set; }
+}
